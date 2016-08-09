90534431b947a4fffe1f300c236804d541bcd7b2
Nathan Walker <walkerrunpdx@gmail.com>
2016-08-09 09:40:22 -0700

    fix: publish with no .ts files

---
+++ index.d.ts
@@ -0,0 +1,9 @@
+import { TNSFontIconPipe, TNSFontIconPurePipe } from './src/app/pipes/fonticon.pipe';
+import { TNSFontIconService } from './src/app/services/fonticon.service';
+export * from './src/app/pipes/fonticon.pipe';
+export * from './src/app/services/fonticon.service';
+declare var _default: {
+    pipes: (typeof TNSFontIconPipe | typeof TNSFontIconPurePipe)[];
+    providers: typeof TNSFontIconService[];
+};
+export default _default;
